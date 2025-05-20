ff3f64a18cc477b222b2015597ca114632365b1f
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-20 08:04:14 +0100

    First functionality done

--- Script/complete.js
+++ Script/complete.js
@@ -1,7 +1,12 @@
  const completedButtons = document.querySelectorAll('.bg-sky-400');
  for(let completedButton of completedButtons){
     completedButton.addEventListener('click',function(){
+        alert("Task has completed");
         completedButton.style.backgroundColor='lightblue';
+        const assignedTask = document.getElementById("task-assigned-number").innerText;
+        const convertedAssignTask = parseInt(assignedTask);
+        let remainingTask = convertedAssignTask - 1;
+        document.getElementById('task-assigned-number').innerText = remainingTask;
     })
  }
  
