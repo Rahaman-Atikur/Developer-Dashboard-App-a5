0e45f6818a4eaac76a3f298269a9fb680f818eaf
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-20 14:35:49 +0100

    Clear History done

--- Script/clearHistory.js
+++ Script/clearHistory.js
@@ -1,4 +1,5 @@
 const clearHistoryButton = document.getElementById("clear-history-btn");
 clearHistoryButton.addEventListener("click",function(){
-    
+  const paragraphDiv=  document.getElementById("done-task-history");
+  paragraphDiv.style.display = "none";
 })
