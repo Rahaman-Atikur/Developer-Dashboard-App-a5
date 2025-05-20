a7922e89c30e2a6632da820e470ddd45f1241d39
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-20 18:33:33 +0100

    Production almost done, dynamic bg color change and clear history part left over

---
+++ Script/backtodesk.js
@@ -0,0 +1,4 @@
+const backToDeskButton = document.getElementById("back-to-desk-btn");
+backToDeskButton.addEventListener("click",function(){
+      window.location.href ="../index.html";
+})
--- Script/blog.js
+++ Script/blog.js
@@ -2,3 +2,4 @@ const clickToBlog = document.getElementById("click-to-blog");
 clickToBlog.addEventListener("click",function(){
     window.location.href ="../blog.html";
 })
+
