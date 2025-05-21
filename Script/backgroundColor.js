4b4c5409b4c3e56e0ec73fe395baa893fcbd49ee
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-21 21:26:25 +0100

    Production ready to deliver

---
+++ Script/backgroundColor.js
@@ -0,0 +1,6 @@
+const themeButton = document.getElementById("theme-btn");
+themeButton.addEventListener("click",function(){
+    const bodyPage = document.getElementById("bg-color");
+    bodyPage.style.backgroundColor='black';
+    
+})
