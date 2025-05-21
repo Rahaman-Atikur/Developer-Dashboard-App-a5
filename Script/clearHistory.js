7cd1489ed8d797f9d8de91cf44c13b9361961635
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-21 20:44:34 +0100

    Production Ready

--- Script/clearHistory.js
+++ Script/clearHistory.js
@@ -1,5 +1,15 @@
 const clearHistoryButton = document.getElementById("clear-history-btn");
 clearHistoryButton.addEventListener("click",function(){
-  const paragraphDiv=  document.getElementById("done-task-history");
-  paragraphDiv.style.display = "none";
+  const paragraphDivOne=  document.getElementById("done-task-history-one");
+  paragraphDivOne.style.display = "none";
+  const paragraphDivTwo=  document.getElementById("done-task-history-two");
+  paragraphDivTwo.style.display = "none";
+  const paragraphDivThree=  document.getElementById("done-task-history-three");
+  paragraphDivThree.style.display = "none";
+  const paragraphDivFour=  document.getElementById("done-task-history-four");
+  paragraphDivFour.style.display = "none";
+  const paragraphDivFive=  document.getElementById("done-task-history-five");
+  paragraphDivFive.style.display = "none";
+  const paragraphDivSix=  document.getElementById("done-task-history-six");
+  paragraphDivSix.style.display = "none";
 })
--- Script/complete.js
+++ Script/complete.js
@@ -11,8 +11,12 @@
         const convertedDoneTask = parseInt(doneTask);
         let totalDoneTask = convertedDoneTask + 1;
         document.getElementById("navbar-twenty-three-btn").innerText = totalDoneTask;
-        // done task history functionality starting
-        const historyOfDoneTask = document.getElementById("done-task-history");
+    })
+ }
+
+  const completeButtonOne = document.getElementById("complete-btn-one");
+  completeButtonOne.addEventListener("click",function(){
+        const historyOfDoneTask = document.getElementById("done-task-history-one");
         const cardOneTask = document.getElementById("card-one-task").innerText;
         let now = new Date();
         let dateTime = now.toLocaleString();
@@ -20,6 +24,71 @@
         historyOfDoneTask.innerHTML=`
         <p> You have completed ${cardOneTask} in ${dateTime}</p> 
         `;
-    })
- }
+ })
+
+ const completeButtonTwo = document.getElementById("complete-btn-two");
+  completeButtonTwo.addEventListener("click",function(){
+        const historyOfDoneTask = document.getElementById("done-task-history-two");
+        const cardTwoTask = document.getElementById("card-two-task").innerText;
+        console.log(cardTwoTask);
+        let now = new Date();
+        let dateTime = now.toLocaleString();
+        console.log(dateTime);
+        historyOfDoneTask.innerHTML=`
+        <p> You have completed ${cardTwoTask} in ${dateTime}</p> 
+        `;
+ })
+
+ const completeButtonThree = document.getElementById("complete-btn-three");
+  completeButtonThree.addEventListener("click",function(){
+        const historyOfDoneTask = document.getElementById("done-task-history-three");
+        const cardThreeTask = document.getElementById("card-three-task").innerText;
+        
+        let now = new Date();
+        let dateTime = now.toLocaleString();
+        console.log(dateTime);
+        historyOfDoneTask.innerHTML=`
+        <p> You have completed ${cardThreeTask} in ${dateTime}</p> 
+        `;
+ })
+
+ const completeButtonFour = document.getElementById("complete-btn-four");
+  completeButtonFour.addEventListener("click",function(){
+        const historyOfDoneTask = document.getElementById("done-task-history-four");
+        const cardFourTask = document.getElementById("card-four-task").innerText;
+        
+        let now = new Date();
+        let dateTime = now.toLocaleString();
+        
+        historyOfDoneTask.innerHTML=`
+        <p> You have completed ${cardFourTask} in ${dateTime}</p> 
+        `;
+ })
+
+ const completeButtonFive = document.getElementById("complete-btn-five");
+  completeButtonFive.addEventListener("click",function(){
+        const historyOfDoneTask = document.getElementById("done-task-history-five");
+        const cardFiveTask = document.getElementById("card-five-task").innerText;
+        
+        let now = new Date();
+        let dateTime = now.toLocaleString();
+        
+        historyOfDoneTask.innerHTML=`
+        <p> You have completed ${cardFiveTask} in ${dateTime}</p> 
+        `;
+ })
+
+ const completeButtonSix = document.getElementById("complete-btn-six");
+  completeButtonSix.addEventListener("click",function(){
+        const historyOfDoneTask = document.getElementById("done-task-history-six");
+        const cardSixTask = document.getElementById("card-six-task").innerText;
+        
+        let now = new Date();
+        let dateTime = now.toLocaleString();
+        
+        historyOfDoneTask.innerHTML=`
+        <p> You have completed ${cardSixTask} in ${dateTime}</p> 
+        `;
+ })
+
  
