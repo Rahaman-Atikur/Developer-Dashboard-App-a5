b5119e67008f22a96b5e89220b1e18cd84abfa56
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-21 11:21:06 +0100

    some changes

--- Script/backtodesk.js
+++ Script/backtodesk.js
@@ -1,4 +1,4 @@
 const backToDeskButton = document.getElementById("back-to-desk-btn");
 backToDeskButton.addEventListener("click",function(){
-      window.location.href ="../index.html";
+      window.location.href ="./index.html";
 })
